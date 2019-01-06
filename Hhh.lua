2d7b5867897767d050c8d784b4d3064ab47ea0a0
phuongbonqb <46427077+phuongbonqb@users.noreply.github.com>
2019-01-07 04:55:29 +0700

    Update Hhh.lua

--- Hhh.lua
+++ Hhh.lua
@@ -1,3 +1,15 @@
-RAINCITYGAMING = load(gg.makeRequest("http://httpbin.org/headers").content)
+gg.clearResults()
 
-pcall(RAINCITYGAMING)
+gg.setRanges(4)
+
+gg.searchNumber("1954047316;1970037078", 4, false, 536870912, 0, -1)
+
+gg.searchNumber("1954047316;1970037078", 4, false, 536870912, 0, -1)
+
+gg.getResults(10)
+
+gg.editAll("9", 4)
+
+gg.toast("☢️☢️☢️ BYPASS Thành Công ☢️☢️☢️")
+
+gg.clearResults()

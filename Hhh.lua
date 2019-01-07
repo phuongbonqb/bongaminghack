14bbe374795f4354b994b2cfbd66a51766d90044
phuongbonqb <46427077+phuongbonqb@users.noreply.github.com>
2019-01-07 05:46:46 +0000

    Update Hhh.lua

--- Hhh.lua
+++ Hhh.lua
@@ -1,20 +1,23 @@
-gg.clearResults()
-
-gg.setRanges(4)
-
-gg.searchNumber("1954047316;1970037078", 4, false, 536870912, 0, -1)
-
-gg.searchNumber("1954047316;1970037078", 4, false, 536870912, 0, -1)
-
-gg.getResults(10)
-
-gg.editAll("9", 4)
-
-gg.toast("☢️☢️☢️ BYPASS Thành Công ☢️☢️☢️")
-gg.toast("☢️☢️☢️ BYPASS Thành Công ☢️☢️☢️")
-gg.toast("☢️☢️☢️ BYPASS Thành Công ☢️☢️☢️")
-gg.toast("☢️☢️☢️ BYPASS Thành Công ☢️☢️☢️")
-gg.toast("☢️☢️☢️ BYPASS Thành Công ☢️☢️☢️")
-gg.toast("☢️☢️☢️ BYPASS Thành Công ☢️☢️☢️")
-
-gg.clearResults()
+function MENUVIP()
+menu1 = gg.multiChoice({
+'🎳 Auto Heashot 95% 📌',
+'🎳 Norecoil (in Game) 📌',
+'🎯 Norecoil 80% (Không Giật 80%)🔫',
+'🎳 Norecoil (1 click ingame) 📌',
+'🎳 Long Hand (Tay Dài) 📌',
+'🎳 High jump (Nhảy Cao) 📌',
+'🎳 Speed AKM (Bốn Tung Lằn) ‼ ‼',
+'🏠⬅️ Menu Chính'},
+nil,'✏️ Youtube: Bon Gaming Hack')
+if menu1 == nil then else
+if menu1[1] == true then autoheashot() end
+if menu1[2] == true then norecoi() end
+if menu1[3] == true then norecoi80() end
+if menu1[4] == true then norecoi1click() end
+if menu1[5] == true then taidai() end
+if menu1[6] == true then nhaycao() end
+if menu1[7] == true then speedak() end
+if menu1[8] == true then HOME() end
+end
+GLWW=-1
+end
